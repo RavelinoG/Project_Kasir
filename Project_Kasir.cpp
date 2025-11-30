07a0dbd89bf961b7669e6b66c1759337f40acb03
RavelinoG <ravelino.h1818@gmail.com>
2025-11-30 13:07:27 +0700

    first commit

---
+++ Project_Kasir.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <conio.h>
+using namespace std;
+
+void ClearScreen(){
+    cout << "\033[2J\033[1;H";
+}
+
+int main (){
+    cout << "hello guys";
+}
